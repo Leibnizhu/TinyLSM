a358b6aa879b40baf63e627103dc10715ab15a6d
Leibniz.Hu <leibnizhu@gmail.com>
2024-03-05 22:11:02 +0800

    feat: Manifest Encoding

--- src/main/scala/io/github/leibnizhu/tinylsm/Manifest.scala
+++ src/main/scala/io/github/leibnizhu/tinylsm/Manifest.scala
@@ -2,18 +2,45 @@ package io.github.leibnizhu.tinylsm
 
 import com.fasterxml.jackson.annotation.JsonSubTypes.Type
 import com.fasterxml.jackson.annotation.{JsonSubTypes, JsonTypeInfo}
+import com.fasterxml.jackson.databind.json.JsonMapper
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import io.github.leibnizhu.tinylsm.compact.CompactionTask
+import io.github.leibnizhu.tinylsm.utils.{ByteArrayReader, ByteArrayWriter}
 
-import java.io.File
+import java.io.*
+import scala.collection.mutable.ListBuffer
 
 class Manifest(file: File) {
+  private val mapper = JsonMapper.builder().addModule(DefaultScalaModule).build()
 
-  def addRecord(): Unit = {
-
+  def addRecord(record: ManifestRecord): Unit = {
+    if (!file.exists()) {
+      file.createNewFile()
+    }
+    val buffer = new ByteArrayWriter()
+    val jsonBytes = mapper.writeValueAsBytes(record)
+    buffer.putUint32(jsonBytes.length)
+    buffer.putBytes(jsonBytes)
+    buffer.putUint32(byteArrayHash(jsonBytes))
+    val writer = new BufferedOutputStream(new FileOutputStream(file, true))
+    writer.write(buffer.toArray)
+    writer.close()
   }
 
-  def recover(): Unit = {
-
+  def recover(): List[ManifestRecord] = {
+    val buffer = new ByteArrayReader(FileInputStream(file).readAllBytes())
+    val records = new ListBuffer[ManifestRecord]()
+    while (buffer.remaining > 0) {
+      val len = buffer.readUint32()
+      val recordBytes = buffer.readBytes(len)
+      val checksum = buffer.readUint32()
+      if (checksum != byteArrayHash(recordBytes)) {
+        throw new IllegalStateException("MANIFEST checksum mismatched!")
+      }
+      val record = mapper.readValue(recordBytes, classOf[ManifestRecord])
+      records += record
+    }
+    records.toList
   }
 }
 
@@ -33,5 +60,5 @@ case class ManifestFlush(flush: Int) extends ManifestRecord
 
 case class ManifestNewMemtable(memtableId: Int) extends ManifestRecord
 
-case class ManifestCompaction(compactionTask: CompactionTask, compactionNewSstId: List[Int]) 
+case class ManifestCompaction(compactionTask: CompactionTask, compactionNewSstId: List[Int])
   extends ManifestRecord
--- src/test/scala/io/github/leibnizhu/tinylsm/ManifestTest.scala
+++ src/test/scala/io/github/leibnizhu/tinylsm/ManifestTest.scala
@@ -2,8 +2,11 @@ package io.github.leibnizhu.tinylsm
 
 import com.fasterxml.jackson.databind.json.JsonMapper
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
-import io.github.leibnizhu.tinylsm.compact.SimpleCompactionTask
+import io.github.leibnizhu.tinylsm.TestUtils.tempDir
+import io.github.leibnizhu.tinylsm.compact.{FullCompactionTask, LeveledCompactionTask, SimpleCompactionTask, TieredCompactionTask}
 import org.scalatest.funsuite.AnyFunSuite
+
+import java.io.File
 
 class ManifestTest extends AnyFunSuite {
 
@@ -18,5 +21,30 @@ class ManifestTest extends AnyFunSuite {
     println(json)
     val record1 = mapper.readValue(json, classOf[ManifestRecord])
     assertResult(record)(record1)
+  }
+
+  test("manifest_encode_decode_test") {
+    val manifestFile = new File(tempDir(), System.currentTimeMillis() + "-MANIFEST")
+    val manifest = new Manifest(manifestFile)
+    val flush = ManifestFlush(123)
+    manifest.addRecord(flush)
+    val memtable = ManifestNewMemtable(456)
+    manifest.addRecord(memtable)
+    val fullCompact = ManifestCompaction(FullCompactionTask(List(1, 2, 3), List(4, 5, 6)), List(1))
+    manifest.addRecord(fullCompact)
+    val simpleCompact = ManifestCompaction(SimpleCompactionTask(None, List(1, 2, 3), 1, List(4, 5, 6), true), List(1))
+    manifest.addRecord(simpleCompact)
+    val tieredCompact = ManifestCompaction(TieredCompactionTask(List((1, List(6)), (2, List(4, 5)), (3, List(1, 2, 3))), true), List(1))
+    manifest.addRecord(tieredCompact)
+    val leveledCompact = ManifestCompaction(LeveledCompactionTask(None, List(1, 2, 3), 1, List(4, 5, 6), true), List(1))
+    manifest.addRecord(leveledCompact)
+
+    val records = manifest.recover()
+    assertResult(6)(records.length)
+    assertResult(List(
+      flush, memtable, fullCompact, simpleCompact, tieredCompact, leveledCompact
+    ))(records)
+
+    manifestFile.delete()
   }
 }
